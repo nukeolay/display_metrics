4a3d0cbb723c5e75ec5eaa7943e5cefe348337c2
nukeolay <nukeolay@gmail.com>
2024-09-20 11:56:21 +0500

    added mmToPixels, pixelsToMm

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -8,6 +8,13 @@ extension ContextExtension on BuildContext {
     return inches * inchToPixelRatio;
   }
 
+  /// Converts millimeters to Flutter logical pixels
+  double mmToPixels(double mm) {
+    final inches = mm / 25.4;
+    return inchesToPixels(inches);
+  }
+
+  @Deprecated('Use mmToPixels(mm)*10 instead')
   /// Converts centimeters to Flutter logical pixels
   double cmToPixels(double cm) {
     final inches = cm / 2.54;
@@ -20,6 +27,12 @@ extension ContextExtension on BuildContext {
     return pixels / inchToPixelRatio;
   }
 
+  /// Converts Flutter logical pixels into millimeters
+  double pixelsToMm(int pixels) {
+    return pixelsToInches(pixels) * 25.4;
+  }
+
+  @Deprecated('Use pixelsToMm(pixels)/10 instead')
   /// Converts Flutter logical pixels into centimeters
   double pixelsToCm(int pixels) {
     return pixelsToInches(pixels) * 2.54;

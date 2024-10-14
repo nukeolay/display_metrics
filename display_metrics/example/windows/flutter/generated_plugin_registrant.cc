7df7d3e6f8f6dc2a4e087a7aa772cc7b7be4519b
Nikolay Kirillov <n.kirillov@doctis.ru>
2024-10-14 22:20:07 +0300

    removed windows from face plugin

--- display_metrics/example/windows/flutter/generated_plugin_registrant.cc
+++ display_metrics/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <display_metrics/display_metrics_plugin_c_api.h>
+#include <display_metrics_windows/display_metrics_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  DisplayMetricsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DisplayMetricsPluginCApi"));
+  DisplayMetricsWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DisplayMetricsWindowsPluginCApi"));
 }
--- display_metrics/example/windows/flutter/generated_plugins.cmake
+++ display_metrics/example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  display_metrics
+  display_metrics_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST

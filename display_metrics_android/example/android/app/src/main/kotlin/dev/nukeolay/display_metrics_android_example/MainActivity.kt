87a1309755c8678b955be9d23b4ae2e9f9dadfce
Nikolay Kirillov <n.kirillov@doctis.ru>
2024-10-14 20:52:15 +0300

    impl display_metrics_android

---
+++ display_metrics_android/example/android/app/src/main/kotlin/dev/nukeolay/display_metrics_android_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.nukeolay.display_metrics_android_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ display_metrics_android/test/display_metrics_android_test.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: avoid_print
+
+void main() {
+  print('---');
+  // TODO(nukeolay): add tests
+  print('---');
+}

6ce6e1f4b0e4a4010294574aa9dcc84c1904daa0
Nikolay Kirillov <n.kirillov@doctis.ru>
2024-10-20 13:19:56 +0300

    removed some tests from face plugin

--- display_metrics/test/src/display_metrics_widget_test.dart
+++ display_metrics/test/src/display_metrics_widget_test.dart
@@ -2,17 +2,39 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:display_metrics/display_metrics.dart';
 
-import 'utils/mock_channel.dart';
 import 'utils/widgets.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  const mockChannel = MockChannel();
-  mockChannel.setup();
-  mockChannel.teardown();
+  
+  const channel = MethodChannel('display_metrics');
+  setUp(
+    () {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async {
+          if (methodCall.method == 'getResolution') {
+            return {'width': 480, 'height': 640};
+          } else if (methodCall.method == 'getSize') {
+            return {'width': 3, 'height': 4};
+          }
+          return null;
+        },
+      );
+    },
+  );
+
+  tearDown(
+    () {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(channel, null);
+    },
+  );
 
   testWidgets(
     'DisplayMetricsWidget',

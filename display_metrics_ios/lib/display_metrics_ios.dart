7c94f74f8409d1295d75bd53f1f13495d3d71ee0
nukeolay <nukeolay@gmail.com>
2025-05-18 19:28:01 +0500

    display_metrics_ios * Updated *display_metrics_platform_interface* dependency (1.1.0)

--- display_metrics_ios/lib/display_metrics_ios.dart
+++ display_metrics_ios/lib/display_metrics_ios.dart
@@ -1,24 +1,11 @@
-import 'dart:ui';
-
-import 'package:display_metrics_platform_interface/display_metrics_method_channel.dart';
 import 'package:display_metrics_platform_interface/display_metrics_platform_interface.dart';
 
 /// The iOS implementation of [DisplayMetricsPlatform].
 ///
 /// This class implements the `package:display_metrics` functionality for iOS.
-class DisplayMetricsIos extends DisplayMetricsPlatform {
+class DisplayMetricsIos extends MethodChannelDisplayMetrics {
   /// Registers this class as the default instance of [DisplayMetricsPlatform].
   static void registerWith() {
-    DisplayMetricsPlatform.instance = MethodChannelDisplayMetrics();
-  }
-
-  @override
-  Future<Size?> getResolution() {
-    return DisplayMetricsPlatform.instance.getResolution();
-  }
-
-  @override
-  Future<Size?> getSize() {
-    return DisplayMetricsPlatform.instance.getSize();
+    DisplayMetricsPlatform.instance = DisplayMetricsIos();
   }
 }

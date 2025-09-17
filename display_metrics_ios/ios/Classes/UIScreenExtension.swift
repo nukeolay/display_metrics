2c0f558cedbc26d17afa2e8e84c20c9729291ca9
Nestor Daniel Flores Moscoso <nestor_halo2@hotmail.com>
2025-09-17 11:01:31 -0600

    Add support for iPhone 17 models in UIScreenExtension

--- display_metrics_ios/ios/Classes/UIScreenExtension.swift
+++ display_metrics_ios/ios/Classes/UIScreenExtension.swift
@@ -78,13 +78,17 @@ public extension UIScreen {
             return 6.1
 
         case "iPhone17,1":                                                 // iPhone 16 Pro
+        case "iPhone18,1":                                                 // iPhone 17 Pro    
+        case "iPhone18,3":                                                 // iPhone 17
             return 6.3
         
         case "iPhone17,2":                                                 // iPhone 16 Pro Max
+        case "iPhone18,2":                                                 // iPhone 17 Pro Max    
             return 6.9
             
         case "iPhone11,4", "iPhone11,6":                       fallthrough // iPhone XS Max
         case "iPhone12,5":                                                 // iPhone 11 Pro Max
+        case "iPhone18,4":                                                 // iPhone Air
             return 6.5
             
         case "iPhone13,4":                                     fallthrough // iPhone 12 Pro Max

a811347e723ef081e6f5740337132febc422dead
Nestor Daniel Flores Moscoso <nestor_halo2@hotmail.com>
2025-09-17 11:55:42 -0600

    Remove print statement in modelIdentifier

--- display_metrics_ios/ios/Classes/UIScreenExtension.swift
+++ display_metrics_ios/ios/Classes/UIScreenExtension.swift
@@ -15,7 +15,6 @@ internal extension UIDevice {
         if let simulatorModelIdentifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] { return simulatorModelIdentifier }
         var sysinfo = utsname()
         uname(&sysinfo) // ignore return value
-        print(String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters))
         return String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
     }()
     
